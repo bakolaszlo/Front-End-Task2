d8422b734c16b40bbb3586f1c051be1dcc720e33
bakolaszlo <bakolaszlo999@gmail.com>
2021-08-04 22:05:59 +0300

    JS part of the table done

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,31 @@
 const myForm = document.getElementById('myForm');
+const table = document.getElementById('table-class');
+
+let startIndex = 0;
+
 
 myForm.addEventListener("submit", (e) =>
 {
     e.preventDefault(); //stops the browser redirect
     console.log("Form has been submited.");
+    let array = document.querySelectorAll('#myForm input, select')
+    appendHTML(array);
 });
 
-let startIndex = 1;
+
+function appendHTML(array)
+{
+    let newRow = table.insertRow();
+    newRow.insertCell().innerHTML=startIndex;
+    startIndex+=1;
+
+    array.forEach(element => {
+        if(element.value!='Register')
+        {
+
+            newRow.insertCell().innerHTML=element.value;
+            console.log(element.value);
+            element.value='';
+        }
+    });
+}
